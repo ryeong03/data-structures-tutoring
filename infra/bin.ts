9ce52077a9dc4aec245cc90724299ee177087ab5
ryeong03 <03.ryeong@gmail.com>
2026-09-17 01:39:32 +0900

    Pin deployment to Seoul region

--- infra/bin.ts
+++ infra/bin.ts
@@ -5,5 +5,5 @@ import { TutoringStack } from './stack.js';
 if(existsSync('.env'))process.loadEnvFile('.env');
 const app=new App();
 new TutoringStack(app,'DataStructuresTutoring',{
-  env:{account:process.env.CDK_DEFAULT_ACCOUNT,region:process.env.CDK_DEFAULT_REGION||'ap-northeast-2'}
+  env:{account:process.env.CDK_DEFAULT_ACCOUNT,region:process.env.AWS_DEPLOY_REGION||'ap-northeast-2'}
 });
